8c93bc70138c3d2ccb4a259cf92a4a7c7ec30ac0
joshmerr <joshmerrill255@gmail.com>
2022-08-29 16:38:17 -0400

    filter for SMB2 data

--- main.go
+++ main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/akamensky/argparse"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
+
+// See if the the 4-8 index of the ApplicationLayer array == the SMB protocol ID
+func checkSMBProtocol(arr []byte) bool {
+	smb2Id := []byte{254, 83, 77, 66}
+	for i, b := range arr {
+		if b != smb2Id[i] {
+			return false
+		}
+	}
+	return true
+}
 
 // Use as interim function to create and feed packets into meat of program
 func initPackets(file string) {
@@ -19,13 +29,15 @@ func initPackets(file string) {
 		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 		for packet := range packetSource.Packets() {
 			appLayer := packet.ApplicationLayer()
+			// If there is an application layer in the packet
 			if appLayer != nil {
-				if strings.Contains(appLayer.Payload(), "mike") {
-
-					fmt.Println("Application Layer found")
-					fmt.Printf("Application Layer data: %s\n", appLayer.Payload())
+				fmt.Println("Application Layer found!")
+				if checkSMBProtocol(appLayer.Payload()[4:8]) {
+					fmt.Println("SMB2 Packet found!")
+					// fmt.Printf("Application Layer data: %s\n", string(appLayer.Payload()))
 					for _, b := range appLayer.Payload() {
-						fmt.Printf("%d ", b)
+						// Looking at direct bytes
+						fmt.Printf("%x ", byte(b))
 					}
 				}
 			}
